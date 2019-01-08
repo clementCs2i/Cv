488a32ff2f22b6f677a5bf17fe191613cf1aa322
unknown <c.savy@cs2i-lorient.fr>
2019-01-08 21:58:15 +0100

    11

--- var/cache/dev/twig/2a/2a3e16f352032b486f54ec657db2aa8d9b04191a8a0cfacb9f6b30dcf6fe74f6.php
+++ var/cache/dev/twig/2a/2a3e16f352032b486f54ec657db2aa8d9b04191a8a0cfacb9f6b30dcf6fe74f6.php
@@ -60,25 +60,54 @@ class __TwigTemplate_a88b5150819da7d2f7a192900fa7eb9f75550aa20cb6462199ac3b7908a
         // line 8
         echo "
 
-
-        <div class=\"CentreDiv\">
-        <div id=\"moncercle\" >
-                <img src=\"";
-        // line 13
+<div id=\"moncercle\" >
+        <img src=\"";
+        // line 11
         echo twig_escape_filter($this->env, $this->extensions['Symfony\Bridge\Twig\Extension\AssetExtension']->getAssetUrl("images/experience.png"), "html", null, true);
-        echo " \"class=\"imageExperience\" >         
+        echo " \"class=\"imageContact\" >
         </div>
-    <span class=\"h2 mb-0 text-black TitreNom\">Expériences <span class=\"ColorRed\">professionnelles</span> </span>
-</div>
-<div class=\"Content \"></div>
-    <p class=\"h5 mb-0 text-black TitreNom\"> <span class=\"ColorRed\">  2016 – 2018</span> 2 ans d'alternance <span class=\"ColorRed\">Master (MS2I) </span>avec l'entreprise ITR <span class=\"ColorRed\"> –> </span> Chef de projet et développeur logiciel</p>
+        
+        <div  class=\"BlocTitre text-center \">
+        
+        <div  class=\"avantTitre\">
+        </div>
+        <p class=\"h1 titre\">Expériences professionels</p>
+        <div  class=\"ApresTitre \">
+        </div>
+        </div>
 
-    <p class=\"h5 mb-0 text-black TitreNom\"> <span class=\"ColorRed\">   2015 – 2016</span> 1 ans d'alternance <span class=\"ColorRed\">License (CS2I) </span>avec l'entreprise ITR <span class=\"ColorRed\"> –> </span> développeur logiciel</p>
-    <p class=\"h5 mb-0 text-black TitreNom\"> <span class=\"ColorRed\">   2015 </span> 2 mois stage <span class=\"ColorRed\">BTS (SIO) </span>avec l'entreprise Urbans <span class=\"ColorRed\"> –> </span> développeur WEB</p>
 
 
+        <div class=\"contenuExperiences\">
 
-</div>
+        <div class=\"row\">
+                <div class=\"col\"> 
+                     <div class=\"contenuUneExperiences\">
+                         <p class=\"h5 mb-0 text-black TitreNom\"> Chef de projet et développeur logiciel</p>
+                         <span class=\"ColorRed\">  2016 – 2018</span>
+                        <p class=\"h5 mb-0 text-black TitreNom\"> Alternance de deux ans <span class=\"ColorRed\">Master (MS2I) </span>avec l'entreprise ITR </p>
+                     </div>
+                </div>
+
+                <div class=\"col\">
+                        <div class=\"contenuUneExperiences\">
+                                <p class=\"h5 mb-0 text-black TitreNom\"> Développeur logiciel</p>
+                                <span class=\"ColorRed\">  2015 – 2016</span>
+                               <p class=\"h5 mb-0 text-black TitreNom\"> Alternance de un ans <span class=\"ColorRed\">Licence (CS2I) </span>avec l'entreprise ITR </p>
+                            </div>
+                </div>
+                    
+                <div class=\"col\">
+                        <div class=\"contenuUneExperiences\">
+                                <p class=\"h5 mb-0 text-black TitreNom\">Développeur WEB</p>
+                                <span class=\"ColorRed\">  2015</span>
+                               <p class=\"h5 mb-0 text-black TitreNom\"> Satge de deux mois <span class=\"ColorRed\">BTS  (SIO) </span>avec l'entreprise ITR </p>
+                            </div>
+                </div>
+            
+        </div>
+        </div>
+
 
 
 ";
@@ -99,7 +128,7 @@ class __TwigTemplate_a88b5150819da7d2f7a192900fa7eb9f75550aa20cb6462199ac3b7908a
 
     public function getDebugInfo()
     {
-        return array (  68 => 13,  61 => 8,  55 => 7,  45 => 5,  39 => 4,  15 => 3,);
+        return array (  66 => 11,  61 => 8,  55 => 7,  45 => 5,  39 => 4,  15 => 3,);
     }
 
     public function getSourceContext()
@@ -113,22 +142,51 @@ class __TwigTemplate_a88b5150819da7d2f7a192900fa7eb9f75550aa20cb6462199ac3b7908a
 {% block body %}
 
 
-
-        <div class=\"CentreDiv\">
-        <div id=\"moncercle\" >
-                <img src=\"{{ asset('images/experience.png') }} \"class=\"imageExperience\" >         
+<div id=\"moncercle\" >
+        <img src=\"{{ asset('images/experience.png') }} \"class=\"imageContact\" >
         </div>
-    <span class=\"h2 mb-0 text-black TitreNom\">Expériences <span class=\"ColorRed\">professionnelles</span> </span>
-</div>
-<div class=\"Content \"></div>
-    <p class=\"h5 mb-0 text-black TitreNom\"> <span class=\"ColorRed\">  2016 – 2018</span> 2 ans d'alternance <span class=\"ColorRed\">Master (MS2I) </span>avec l'entreprise ITR <span class=\"ColorRed\"> –> </span> Chef de projet et développeur logiciel</p>
+        
+        <div  class=\"BlocTitre text-center \">
+        
+        <div  class=\"avantTitre\">
+        </div>
+        <p class=\"h1 titre\">Expériences professionels</p>
+        <div  class=\"ApresTitre \">
+        </div>
+        </div>
 
-    <p class=\"h5 mb-0 text-black TitreNom\"> <span class=\"ColorRed\">   2015 – 2016</span> 1 ans d'alternance <span class=\"ColorRed\">License (CS2I) </span>avec l'entreprise ITR <span class=\"ColorRed\"> –> </span> développeur logiciel</p>
-    <p class=\"h5 mb-0 text-black TitreNom\"> <span class=\"ColorRed\">   2015 </span> 2 mois stage <span class=\"ColorRed\">BTS (SIO) </span>avec l'entreprise Urbans <span class=\"ColorRed\"> –> </span> développeur WEB</p>
 
 
+        <div class=\"contenuExperiences\">
 
-</div>
+        <div class=\"row\">
+                <div class=\"col\"> 
+                     <div class=\"contenuUneExperiences\">
+                         <p class=\"h5 mb-0 text-black TitreNom\"> Chef de projet et développeur logiciel</p>
+                         <span class=\"ColorRed\">  2016 – 2018</span>
+                        <p class=\"h5 mb-0 text-black TitreNom\"> Alternance de deux ans <span class=\"ColorRed\">Master (MS2I) </span>avec l'entreprise ITR </p>
+                     </div>
+                </div>
+
+                <div class=\"col\">
+                        <div class=\"contenuUneExperiences\">
+                                <p class=\"h5 mb-0 text-black TitreNom\"> Développeur logiciel</p>
+                                <span class=\"ColorRed\">  2015 – 2016</span>
+                               <p class=\"h5 mb-0 text-black TitreNom\"> Alternance de un ans <span class=\"ColorRed\">Licence (CS2I) </span>avec l'entreprise ITR </p>
+                            </div>
+                </div>
+                    
+                <div class=\"col\">
+                        <div class=\"contenuUneExperiences\">
+                                <p class=\"h5 mb-0 text-black TitreNom\">Développeur WEB</p>
+                                <span class=\"ColorRed\">  2015</span>
+                               <p class=\"h5 mb-0 text-black TitreNom\"> Satge de deux mois <span class=\"ColorRed\">BTS  (SIO) </span>avec l'entreprise ITR </p>
+                            </div>
+                </div>
+            
+        </div>
+        </div>
+
 
 
 {% endblock %}
